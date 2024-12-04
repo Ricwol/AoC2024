3770c01707dc7ed4046a5e1b2e942f644400f925
Ricwol <cedric.r.wolffs@gmail.com>
2024-12-04 01:53:39 +0000

    Add day 3 solution part 2

--- challenges/day_03/mull_it_over.py
+++ challenges/day_03/mull_it_over.py
@@ -1,15 +1,22 @@
 import operator as op
 import re
 
+from collections.abc import Generator
 from functools import reduce
 
-REGEX = r'mul\(\d{1,3},\d{1,3}\)'
+REGEX_MUL = r'mul\(\d{1,3},\d{1,3}\)'
+REGEX_ENABLE = '|'.join((REGEX_MUL, r'don\'t\(\)', r'do\(\)'))
 
 
 def main() -> None:
     corrupted_memory = read_input('input.txt')
-    result = scan(corrupted_memory, REGEX)
+    result = scan(corrupted_memory, REGEX_MUL)
     print(f'The result of the multiplication: {result:_}')
+
+    enabled_results = scan(corrupted_memory, REGEX_ENABLE)
+    print(
+        f'The result of only the enabled multiplication: {enabled_results:_}'
+    )
 
 
 def read_input(filename: str) -> str:
@@ -18,11 +25,23 @@ def read_input(filename: str) -> str:
 
 
 def scan(memory: str, regex: str) -> int:
-    return sum(mul(instruction) for instruction in re.findall(regex, memory))
+    return sum(map(mul, filter_instructions(memory, regex)))
 
 
 def mul(instruction: str) -> int:
     return reduce(op.mul, map(int, re.findall(r'\d+', instruction)))
+
+
+def filter_instructions(memory: str, regex: str) -> Generator[str, None, None]:
+    enabled = True
+    for instruction in re.findall(regex, memory):
+        if instruction == 'don\'t()':
+            enabled = False
+        elif instruction == 'do()':
+            enabled = True
+        
+        if enabled and instruction.startswith('m'):
+            yield instruction
 
 
 if __name__ == '__main__':
--- tests/test_mull_it_over.py
+++ tests/test_mull_it_over.py
@@ -1,9 +1,11 @@
 import pytest
 
 from challenges.day_03.mull_it_over import (
+    filter_instructions,
     mul,
     scan,
-    REGEX,
+    REGEX_MUL,
+    REGEX_ENABLE
 )
 
 
@@ -18,9 +20,40 @@ def test_mul_raises_TypeError():
 
 def test_get_instructions():
     memory = 'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'
-    assert 161 == scan(memory, REGEX)
+    assert 161 == scan(memory, REGEX_MUL)
 
 
 def test_get_instructions_nums_bigger_3_digits():
     memory = 'xmul(2,4321)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8765,5))'
-    assert 113 == scan(memory, REGEX)
+    assert 113 == scan(memory, REGEX_MUL)
+
+
+def test_regex():
+    expected = r'mul\(\d{1,3},\d{1,3}\)|don\'t\(\)|do\(\)'
+    assert expected == REGEX_ENABLE
+
+
+def test_filter_instructions_simple():
+    memory = 'xmul(2,4)&mul[3,7]!^dont()_mul(5,5)+mul(32,64](mul(11,8)undo(]?mul(8,5))'
+    expected = ['mul(2,4)', 'mul(5,5)', 'mul(11,8)', 'mul(8,5)']
+
+    assert expected == list(filter_instructions(memory, REGEX_ENABLE))
+
+
+def test_filter_instructions_enabled():
+    memory = 'xmul(2,4)&mul[3,7]!^dont()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))'
+    expected = ['mul(2,4)', 'mul(5,5)', 'mul(11,8)', 'mul(8,5)']
+
+    assert expected == list(filter_instructions(memory, REGEX_ENABLE))
+
+
+def test_filter_instructions_disabled():
+    memory = 'xmul(2,4)&mul[3,7]!^don\'t()_mul(5,5)+mul(32,64](mul(11,8)undo(]?mul(8,5))'
+    assert ['mul(2,4)'] == list(filter_instructions(memory, REGEX_ENABLE))
+
+
+def test_filter_instructions():
+    memory = 'xmul(2,4)&mul[3,7]!^don\'t()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))'
+    expected = ['mul(2,4)', 'mul(8,5)']
+
+    assert expected == list(filter_instructions(memory, REGEX_ENABLE))
